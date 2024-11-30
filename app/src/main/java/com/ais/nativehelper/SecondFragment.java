880d5cbbb76136a12219803fc0c6469420506a7f
naveen <naveen92mg@gmail.com>
2024-11-30 16:53:34 +0530

    home activity pro5

--- app/src/main/java/com/ais/nativehelper/SecondFragment.java
+++ app/src/main/java/com/ais/nativehelper/SecondFragment.java
@@ -32,6 +32,9 @@ public class SecondFragment extends Fragment {
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
+
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
